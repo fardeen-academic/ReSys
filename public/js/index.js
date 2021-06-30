4ca626fc049dc41b57d0aeea881ea0c1a980f45f
fardeen Ehsan <fardeen.es7@gmail.com>
2021-06-30 23:23:29 +0600

    minor changes

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,25 @@ var db = firebase.firestore();
 var uid;
 var role = "student";
 var user = firebase.auth().currentUser;
+
+var lform = document.getElementById('loginform');
+var prform = document.getElementById('p_resetform');
+var title = document.getElementById('title');
+prform.style.display = "none";
+
+function showprform(){
+    lform.style.display = 'none';
+    prform.style.display = 'block';
+    title.innerHTML = "Password Reset";
+}
+
+function showlform(){
+    lform.style.display = 'block';
+    prform.style.display = 'none';
+    title.innerHTML("Student Login");
+}
+
+
 firebase.auth().onAuthStateChanged(function(user){
     if(user){
         var uid = user.uid;
@@ -26,4 +45,17 @@ function login(){
         window.alert("Error: "+errorMessage);
         console.log("Error");
     });
+}
+
+function resetpassword(){
+    var email = document.getElementById("resetemail").value;
+    firebase.auth().sendPasswordResetEmail(email)
+  .then(() => {
+    window.alert("Password reset link sent to your email!");
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    window.alert("Error "+errorCode+": "+errorMessage);
+  });
 }
